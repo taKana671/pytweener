aede17db4601ff7cab8b80bff59d4de47f73f478
taKana671 <kanae5321@gmail.com>
2025-06-12 19:13:24 +0900

    First commit

---
+++ tests/test_ease.py
@@ -0,0 +1,19 @@
+import unittest
+
+from ease import Ease
+
+
+class TestEase(unittest.TestCase):
+
+    def test_in_sine(self):
+        pass
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
