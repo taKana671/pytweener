2e262082f9a73d586de4175c24cd107ebd11e154
taKana671 <kanae5321@gmail.com>
2025-06-13 14:20:20 +0900

    Add comment.

--- ease.py
+++ ease.py
@@ -4,6 +4,7 @@ import math
 class Ease:
     """
     easing functions are based on https://easings.net/.
+    # http://robertpenner.com/easing/
     """
 
     @staticmethod
@@ -15,7 +16,7 @@ class Ease:
         """The variable x represents the absolute progress of the animation
            in the bounds of 0 (beginning of the animation) and 1 (end of animation).
         """
-        return 1 - math.cos((x * math.pi) / 2)
+        return x if x == 1.0 else 1 - math.cos((x * math.pi) / 2)
 
     @staticmethod
     def out_sine(x):
@@ -35,8 +36,8 @@ class Ease:
 
     @staticmethod
     def in_out_cubic(x):
-        return 4 * math.pow(x, 3) if x < 0.5 else \
-            1 - math.pow(-2 * x + 2, 3) / 2
+        return 4 * math.pow(x, 3) if x < 0.5 \
+            else 1 - math.pow(-2 * x + 2, 3) / 2
 
     @staticmethod
     def in_quint(x):
@@ -48,7 +49,8 @@ class Ease:
 
     @staticmethod
     def in_out_quint(x):
-        return 16 * math.pow(x, 5) if x < 0.5 else 1 - math.pow(-2 * x + 2, 5) / 2
+        return 16 * math.pow(x, 5) if x < 0.5 \
+            else 1 - math.pow(-2 * x + 2, 5) / 2
 
     @staticmethod
     def in_circ(x):
@@ -60,8 +62,8 @@ class Ease:
 
     @staticmethod
     def in_out_circ(x):
-        return (1 - math.sqrt(1 - math.pow(2 * x, 2))) / 2 if x < 0.5 else \
-            (math.sqrt(1 - math.pow(-2 * x + 2, 2)) + 1) / 2
+        return (1 - math.sqrt(1 - math.pow(2 * x, 2))) / 2 if x < 0.5 \
+            else (math.sqrt(1 - math.pow(-2 * x + 2, 2)) + 1) / 2
 
     @staticmethod
     def in_elastic(x):
@@ -89,7 +91,7 @@ class Ease:
         if x < 0.5:
             return -(math.pow(2, 20 * x - 10) * math.sin((20 * x - 11.125) * c5)) / 2
 
-        return (math.pow(2, -20 * x + 10) * math.sin((20 * x - 11.125) * c5)) / 2 + 1
+        return math.pow(2, -20 * x + 10) * math.sin((20 * x - 11.125) * c5) / 2 + 1
 
     @staticmethod
     def in_quad(x):
@@ -140,22 +142,22 @@ class Ease:
 
         c1 = 1.70158
         c3 = c1 + 1
-        return c3 * x * x * x - c1 * x * x
+        return c3 * math.pow(x, 3) - c1 * math.pow(x, 2)
 
     @staticmethod
     def out_back(x):
         c1 = 1.70158
         c3 = c1 + 1
 
-        return 1 + c3 * math.pow(x - 1, 3) + c1 * math.pow(x - 1, 2)
+        return x if x == 0.0 else 1 + c3 * math.pow(x - 1, 3) + c1 * math.pow(x - 1, 2)
 
     @staticmethod
     def in_out_back(x):
         c1 = 1.70158
         c2 = c1 * 1.525
 
-        return (math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 if x < 0.5 else \
-            (math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2
+        return (math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 if x < 0.5 \
+            else (math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2
 
     @staticmethod
     def in_bounce(x):
@@ -169,13 +171,13 @@ class Ease:
         if x < 1 / d1:
             return n1 * x * x
         elif x < 2 / d1:
-            return n1 * ((x - 1.5) / d1) * x + 0.75
+            return n1 * math.pow(x - 1.5 / d1, 2) + 0.75
         elif (x < 2.5 / d1):
-            return n1 * ((x - 2.25) / d1) * x + 0.9375
+            return n1 * math.pow(x - 2.25 / d1, 2) + 0.9375
         else:
-            return n1 * ((x - 2.625) / d1) * x + 0.984375
+            return n1 * math.pow(x - 2.625 / d1, 2) * x + 0.984375
 
     @staticmethod
     def in_out_bounce(x):
-        return (1 - Ease.out_bounce(1 - 2 * x)) / 2 if x < 0.5 else \
-            (1 + Ease.out_bounce(2 * x - 1)) / 2
+        return (1 - Ease.out_bounce(1 - 2 * x)) / 2 if x < 0.5 \
+            else (1 + Ease.out_bounce(2 * x - 1)) / 2
